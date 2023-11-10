db26c37cca6d74d36f6f885757c5a38756edba32
Agarwal, Aaryaman <aagarwal485@gatech.edu>
2023-11-10 11:49:16 -0500

    Made factory for enemies

---
+++ app/src/main/java/com/example/demo_2340/EnemiesFactory.java
@@ -0,0 +1,20 @@
+package com.example.demo_2340;
+
+public class EnemiesFactory {
+    public static Enemies buildEnemies(String type) {
+        if (type == null || type.isEmpty()) {
+            return null;
+        } else {
+            if (type.equals("Sprite")) {
+                return new Sprite();
+            } else if (type.equals("Heavy")) {
+                return new Heavy1();
+            } else if (type.equals("Heavy2")) {
+                return new Heavy2();
+            } else if (type.equals("Heavy3")) {
+                return new Heavy3();
+            }
+            throw new IllegalArgumentException("Unknown Enemy " + type);
+        }
+    }
+}
