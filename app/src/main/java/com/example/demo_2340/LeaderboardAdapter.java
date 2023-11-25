98624b429aaa10d82e433c8153ca6a8afead866f
greidy3 <grant.reidy@gatech.edu>
2023-11-25 13:03:40 -0500

    NO WAY AM CODING GOD GOT IT TO WORK

--- app/src/main/java/com/example/demo_2340/LeaderboardAdapter.java
+++ app/src/main/java/com/example/demo_2340/LeaderboardAdapter.java
@@ -13,9 +13,6 @@ public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.
 
     private static LeaderboardAdapter instance;
 
-
-
-
     private List<LeaderboardItem> leaderboardData;
     private LeaderboardAdapter(List<LeaderboardItem> leaderboardData) {
         this.leaderboardData = leaderboardData;
